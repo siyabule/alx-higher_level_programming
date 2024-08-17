bb84350515090ccd01c8cb4563a3a95a2ba71e9d
siyabule <139670834+siyabule@users.noreply.github.com>
2024-08-17 01:41:58 -0700

    completed task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for number in range(0, 101):
-        if number % 3 == 0:
-            print("Fizz", end=" ")
-        elif number % 5 == 0:
-            print("Buzz", end=" ")
-        elif number % 15 == 0:
+    for i in range(1, 101):
+        if i % 15 == 0:
             print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
         else:
-            print(number, end=" ")
+            print(i, end=" ")
