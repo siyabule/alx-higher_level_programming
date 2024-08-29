7fc4987b242566041b96ca5c1800ddae81c74cb6
siyabule <139670834+siyabule@users.noreply.github.com>
2024-08-29 13:02:07 -0700

    completed task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def islower(c):
-    """Check for lowercase characters."""
+    """Check for lowercase characters.
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
-        return False
+        return False"""
+    for i in range(97, 123):
+        if chr(i) == c:
+            return True
+        else:
+            return False
